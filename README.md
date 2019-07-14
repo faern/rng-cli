9815d66eb846240269bb87873fc3552b9ced03c4
Linus Färnstrand <faern@faern.net>
2019-07-14 20:41:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# rng - A random number generator CLI tool.
+
+Contains a number of (pseudo) random number generator algorithms. Given one of these it prints an
+infinite stream of bytes generated from that algorithm to stdout.
+
+In unix terms it can basically be viewed as the equivalent of `cat /dev/urandom` but with a
+selection of different (mostly user-space) PRNG algorithms to choose from.
+
+This tool is basically a CLI frontend for the awesome `rand` crate.
+
+# How to use
+
+This tool is very simple and have very few arguments:
+```
+rng [--seed <seed>] [<algorithm>]
+```
+
+If no arguments are given it uses the default algorithm and seeds it from the operating system.
+You might want to change the algorithm in order to fit your needs. Maybe you need a faster algorithm
+that is not cryptographically secure for example.
+
+The `--seed <seed>` argument initializes the random number algorithm with the given `<seed>` instead
+of obtaining some entropy from the operating system. You don't want to use this for any
+cryptographical purposes. Giving a seed can be useful when you need determinism and must be able
+to produce identical data in multiple runs.
+
+## Example
+
+We try using the PCG algoritm a few times. Here we see that without a seed it produces different
+output each time, and with a seed it produces the same data as long as the seed is the same.
+
+```bash
+$ rng pcg | dd count=1024 | shasum
+ae148d8b54ee544a50833c2a6915b0fca4cb95ed -
+$ rng pcg | dd count=1024 | shasum
+65a5651a2b1201adbe8bf60d9f7a2b940f6e188b -
+$ rng pcg --seed 6 | dd count=1024 | shasum
+fc14baccbc847339408457335dc67bc4c9785185 -
+$ rng pcg --seed 6 | dd count=1024 | shasum
+fc14baccbc847339408457335dc67bc4c9785185 -
+$ rng pcg --seed 7 | dd count=1024 | shasum
+c7a169d195c395867396d16d70c1a3c19f63b5cc -
+```
+
+# Why?
+
+This tool was invented because I needed to benchmark IO (both filesystem and network) on Linux.
+Reading from `/dev/urandom` was too slow, the machine spent too much time computing the actual data
+and too little time performing the IO I wanted to test. On the other hand, getting data from
+`/dev/zero` is fast! But any compression (common in both network protocols and filesystems) will be
+able to optimize this stream down to almost nothing. And again, the actual IO is not being properly
+benchmarked.
+
+So what I needed was a very fast stream of incompressible data. The `rand` crate provides a unified
+interface to a number of different random number algorithms. Some of them suiting my use case.
---
+++ src/main.rs
@@ -0,0 +1,151 @@
+use rand::{RngCore, SeedableRng};
+use std::{
+    fmt,
+    io::{self, Write},
+};
+use structopt::StructOpt;
+
+/// The number of bytes to handle in each generate-print iteration.
+const BUFFER_SIZE: usize = 64 * 1024;
+
+// We select PCG algorithm depending on platform. In order to get the best performance possible.
+// This code is copied from the implementation of `SmallRng` in the `rand` crate.
+// `SmallRng` does not guarantee it will always stick to PCG, otherwise we could use that wrapper
+// directly.
+#[cfg(all(not(target_os = "emscripten"), target_pointer_width = "64"))]
+type PcgRng = rand_pcg::Pcg64Mcg;
+#[cfg(not(all(not(target_os = "emscripten"), target_pointer_width = "64")))]
+type PcgRng = rand_pcg::Pcg32;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "rng",
+    about = "
+        A random data generator CLI tool.
+
+        Contains a number of (pseudo) random number generator algorithms. Given one of these it
+        prints an infinite stream of bytes generated from that algorithm to stdout.
+    ",
+    rename_all = "kebab-case"
+)]
+struct Opt {
+    /// Which random number generator algorithm to use. All of these are user-space PRNGs, except
+    /// the "os" one. All user-space generators are seeded with entropy from the operating system,
+    /// unless the --seed argument is given.
+    /// Possible values are:
+    ///
+    /// * default - The default user-space PRNG. Considered cryptographically secure.
+    ///
+    /// * hc - A cryptographically secure random number generator that uses the HC-128 algorithm.
+    ///
+    /// * chacha[8,12,20] - A cryptographically secure random number generator that uses the ChaCha
+    /// algorithm. Uses 8, 12 or 20 rounds. Defaults to 20 rounds if the number is not specified.
+    ///
+    /// * xorshift - The Xorshift algorithm is NOT suitable for cryptographic purposes, but is fast.
+    ///
+    /// * pcg - This algorithm is NOT considered cryptographically secure. has good statistical
+    /// quality and is usually the fastest algorithm in this tool.
+    ///
+    /// * os - A random number generator that retrieves randomness from the operating system.
+    /// Usually cryptograhically secure, but depends on the OS. Usually much slower than the
+    /// user-space PRNGs. The --seed argument can't be used with this algorithm, as the operating
+    /// system is in control of providing the data.
+    algorithm: Option<Algorithm>,
+
+    /// Optionally seed the random number generator algorithm with a given 64 bit unsigned integer.
+    /// This makes the output of the program identical for each run with the same algorithm and
+    /// same seed. If this argument is not given, the PRNG will be seeded from the operating system.
+    /// Specifying a seed is NOT recommended for cryptographic use.
+    #[structopt(long)]
+    seed: Option<u64>,
+}
+
+#[derive(Debug)]
+enum Algorithm {
+    Default,
+    Hc,
+    ChaCha8,
+    ChaCha12,
+    ChaCha20,
+    XorShift,
+    Pcg,
+    Os,
+}
+
+impl std::str::FromStr for Algorithm {
+    type Err = ParseAlgorithmError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "default" => Ok(Algorithm::Default),
+            "hc" => Ok(Algorithm::Hc),
+            "chacha" | "chacha20" => Ok(Algorithm::ChaCha20),
+            "chacha8" => Ok(Algorithm::ChaCha8),
+            "chacha12" => Ok(Algorithm::ChaCha12),
+            "xorshift" => Ok(Algorithm::XorShift),
+            "pcg" => Ok(Algorithm::Pcg),
+            "os" => Ok(Algorithm::Os),
+            _ => Err(ParseAlgorithmError(())),
+        }
+    }
+}
+
+/// Initializes a given PRNG. Either from entropy provided by the OS, or from the given u64 seed.
+macro_rules! init_rng {
+    ($rng:ty, $seed:expr) => {
+        match $seed {
+            None => <$rng>::from_entropy(),
+            Some(seed) => <$rng>::seed_from_u64(seed),
+        }
+    };
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    let seed = opt.seed;
+
+    match opt.algorithm.unwrap_or(Algorithm::Default) {
+        Algorithm::Default => generate(init_rng!(rand::rngs::StdRng, seed)),
+        Algorithm::Hc => generate(init_rng!(rand_hc::Hc128Rng, seed)),
+        Algorithm::ChaCha8 => generate(init_rng!(rand_chacha::ChaCha8Rng, seed)),
+        Algorithm::ChaCha12 => generate(init_rng!(rand_chacha::ChaCha12Rng, seed)),
+        Algorithm::ChaCha20 => generate(init_rng!(rand_chacha::ChaCha20Rng, seed)),
+        Algorithm::XorShift => generate(init_rng!(rand_xorshift::XorShiftRng, seed)),
+        Algorithm::Pcg => generate(init_rng!(PcgRng, seed)),
+        Algorithm::Os => {
+            if seed.is_some() {
+                eprintln!("WARNING: --seed is ignored when used with the OS PRNG");
+            }
+            generate(rand::rngs::OsRng);
+        }
+    }
+}
+
+/// Given a random number generator, prints the output of it to stdout forever, or until there
+/// is an error printing to stdout. Usually because the pipe has closed.
+fn generate(mut rng: impl RngCore) {
+    let stdout = io::stdout();
+    let mut stdout_lock = stdout.lock();
+
+    let mut buf = [0u8; BUFFER_SIZE];
+    loop {
+        rng.fill_bytes(&mut buf);
+        if stdout_lock.write_all(&buf).is_err() {
+            break;
+        }
+    }
+}
+
+#[derive(Debug)]
+struct ParseAlgorithmError(());
+
+impl fmt::Display for ParseAlgorithmError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Invalid algorithm. See --help for a list of valid options."
+        )
+    }
+}
+
+impl std::error::Error for ParseAlgorithmError {}
